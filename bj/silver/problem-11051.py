ebe5f28091181ba4d9a050d26a91f16718469aa4
ysysss <54470484+alsdbtjd0103@users.noreply.github.com>
2022-04-15 21:04:36 +0900

    Create problem-11051.py

---
+++ bj/silver/problem-11051.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+
+n,k=map(int,input().rstrip().split())
+a,b=1,1
+for i in range(k):
+    a=(a*(n-i))
+for j in range(k):
+    b=b*(k-j)
+    
+print((a//b)%10007)
