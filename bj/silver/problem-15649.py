91cf2431b70abff7b1a55949fffddd94a1e62bd2
ysyss <alsdbtjd0103@naver.com>
2022-11-09 15:54:20 +0900

    solved

--- bj/silver/problem-15649.py
+++ bj/silver/problem-15649.py
@@ -1,9 +1,26 @@
-from itertools import permutations
 n,m=map(int,input().split())
-a=[i for i in range(1,n+1)]
+a=[]
 
-p=permutations(a,m)
-for per in p:
-    for k in per:
-        print(k,end=' ')
-    print('')
+def dfs():
+    if len(a)==m:
+        print(' '.join(map(str,a)))
+        return
+    for i in range(1,n+1):
+        if i in a:
+            continue
+        a.append(i)
+        dfs()
+        a.pop()
+
+dfs()
+
+
+# from itertools import permutations
+# n,m=map(int,input().split())
+# a=[i for i in range(1,n+1)]
+
+# p=permutations(a,m)
+# for per in p:
+#     for k in per:
+#         print(k,end=' ')
+#     print('')
