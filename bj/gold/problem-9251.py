df6e9abf81b15501df3fce734e469e6eeef75467
ysyss <alsdbtjd0103@naver.com>
2022-11-12 22:58:02 +0900

    asd

--- bj/gold/problem-9251.py
+++ bj/gold/problem-9251.py
@@ -1,14 +1,9 @@
-a=list(input())
-b=list(input())
-a_len=len(a)
-b_len=len(b)
-length=max(a_len,b_len)
-d=[0]*length    # 인덱스까지의 최장 부분수열의 길이
-if a_len>=b_len:
-    for i in range(a_len):
-        for j in range(b_len):
-            if a[i]==b[j]:
-                d[j]+=1
-            else:
-                
-                
+a=input()
+b=input()
+if len(a)>len(b):
+    a,b=b,a
+d1=[1 for i in range(len(a)+1)]
+d2=[1 for i in range(len(b)+1)]
+
+
+
---
+++ bj/silver/problem-2941.py
@@ -0,0 +1,8 @@
+a=input()
+alpha=['c=','c-','dz=','lj','nj','s=','z=']
+cnt=0
+
+for al in alpha:
+    print(cnt)
+    cnt+=a.count(al)
+print(cnt)
