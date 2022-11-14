738b0199d9dbf02cb50f8ab57cb16a8f57c98674
ysyss <alsdbtjd0103@naver.com>
2022-11-14 15:21:39 +0900

    9251

--- bj/gold/problem-9251.py
+++ bj/gold/problem-9251.py
@@ -1,9 +1,14 @@
 a=input()
 b=input()
-if len(a)>len(b):
-    a,b=b,a
-d1=[1 for i in range(len(a)+1)]
-d2=[1 for i in range(len(b)+1)]
+ans=-1
+dp=[[0 for i in range(len(b)+1)] for j in range(len(a)+1)]
 
-
-
+for i in range(1,len(a)+1):
+    for j in range(1,len(b)+1):
+        if a[i-1]==b[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        ans=max(ans,dp[i][j])
+        
+print(ans)
