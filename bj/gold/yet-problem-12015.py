f116e65e13e2b7b98d97a9d9261f61f117e25581
ysyss <alsdbtjd0103@naver.com>
2022-11-20 16:32:40 +0900

    12015

--- bj/gold/yet-problem-12015.py
+++ bj/gold/yet-problem-12015.py
@@ -1,11 +1,28 @@
 import sys
 input=sys.stdin.readline
+from bisect import bisect_left
 n=int(input())
-a=list(map(int,input().rstrip().split()))
-a.sort()
-count=0
-start=1
-end=n
-while start<=end:
-    mid=(start+end)//2
+a=list(map(int,input().split()))
+
+result=[]
+
+for i in range(n):
+    if not result:
+        result.append(a[i])
+    else:
+        if a[i]>result[-1]:
+            result.append(a[i])
+        else:
+            index=bisect_left(result,a[i])
+            result[index]=a[i]
     
+print(len(result))
+
+
+
+
+
+
+
+
+
