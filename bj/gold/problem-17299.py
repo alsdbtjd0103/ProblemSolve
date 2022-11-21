f66ec7fea8746e60995176058d036d71800f1d84
ysyss <alsdbtjd0103@naver.com>
2022-11-22 07:23:04 +0900

    17299

---
+++ bj/gold/problem-17299.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+n = int(input())
+a = list(map(int, input().split()))
+b = defaultdict(int)
+for i in a:
+    b[i] += 1
+stack = []
+result = []
+for i in range(len(a)):
+
+    while stack:
+        index = stack[-1]
+        if b[a[index]] < b[a[i]]:
+            stack.pop()
+            result.append((index, a[i]))
+        else:
+            break
+    stack.append(i)
+
+for s in stack:
+    result.append((s, -1))
+
+result.sort()
+for i in result:
+    print(i[1], end=' ')
